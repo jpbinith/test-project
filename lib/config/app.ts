4b38517dcabff085fa56129fdd4a777ad2a96854
jpbinith <jpbinith@gmail.com>
2020-04-11 06:11:05 +0530

    routes added

--- lib/config/app.ts
+++ lib/config/app.ts
@@ -1,16 +1,26 @@
 import * as express from "express";
 import * as bodyParser from "body-parser";
+import { TestRoutes } from "../routes/test_routes";
+import { CommonRoutes } from "../routes/common_routes";
 class App {
+
    public app: express.Application;
+   private test_routes: TestRoutes = new TestRoutes();
+   private common_routes: CommonRoutes = new CommonRoutes();
+
    constructor() {
       this.app = express();
       this.config();
+      this.test_routes.route(this.app);
+      this.common_routes.route(this.app);
    }
-private config(): void {
+
+   private config(): void {
       // support application/json type post data
       this.app.use(bodyParser.json());
       //support application/x-www-form-urlencoded post data
       this.app.use(bodyParser.urlencoded({ extended: false }));
    }
+
 }
 export default new App().app;
---
+++ lib/routes/common_routes.ts
@@ -0,0 +1,12 @@
+import { Application, Request, Response } from 'express';
+
+export class CommonRoutes {
+    public route(app: Application) {
+
+        // Mismatch URL
+        app.all('*', function (req: Request, res: Response) {
+            res.status(404).send({ error: true, message: 'Check your URL please' });
+        });
+
+    }
+}
